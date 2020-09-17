833ddb3bfdd85fb3af254becdc97642e3e087c0e
Alex <Lomar1o1990@gmail.com>
2020-09-17 15:43:45 +0300

    Clear version, not finall

--- file.py
+++ file.py
@@ -7,7 +7,7 @@ from datetime import timedelta, date
 
 
 def convert_date(_date):
-    # Добавляем к введенной дате год
+    # Конвертируем полученную дату из дд.мм в гггг-мм-дд
     today = date.today()
     try:
         day, month = get_date(_date)
@@ -17,9 +17,9 @@ def convert_date(_date):
             today = today + timedelta(days=1)
     except AttributeError:
         return today
+    # Если введенная дата прошла, то переносим ее на следующий год
     try:
         if today > today.replace(day=day, month=month, year=today.year):
-            # Если дата уже прошла, то переходит на следующий год
             today = today.replace(day=day, month=month, year=today.year + 1)
         else:
             today = today.replace(day=day, month=month, year=today.year)
@@ -28,48 +28,63 @@ def convert_date(_date):
     return str(today)
 
 def get_date(_date):
+    # Из введенной даты получаем день и месяц в виде числа
     day, month = list(map(int, _date.split('.')))
     return day, month
 
 
 class Base(ABC):
+    """
 
+    Абстрактный класс для создания общего интерфейся работы с БД.
+
+    write_to_database() - записывает введенную пользователем информацию в БД,
+    read_from_database() - читает информацию из БД.
+
+    """
     def __init__(self):
         self.today = date.today()
 
     @abstractmethod
-    def write(self, task, _date):
+    def write_to_database(self, task, _date):
         pass
 
     @abstractmethod
-    def read(self, _date=None):
+    def read_from_database(self, _date=None):
         pass
 
 
 class FileJson(Base):
 
     def __init__(self):
-        self.path_to_file = os.path.join('', 'manager1.data')
-        # self.path_to_file = os.path.join(tempfile.gettempdir(), 'manager.data')
+        # Задаем путь до файла, в котором хранится информация
+        self.path_to_file = os.path.join(tempfile.gettempdir(), 'manager.data')
         super().__init__()
 
-    def write(self, task, _date):
+    def write_to_database(self, task, _date):
         task = {_date: task}
-        if os.path.exists(self.path_to_file):
+        # Читаем файл, если он не существует, создаем пустой словарь
+        try:
             data = self._read_file()
+        except json.JSONDecoderError:
+            data = dict()
+        # добавляем в словарь информацию {дата: [дело]}
         with open(self.path_to_file, 'w') as f:
             for key, value in task.items():
                 if key not in data:
                     data[key] = list()
                 data[key].append(value)
             json.dump(data, f)
-        return f'Данные успешно занесены в журнал'
 
-    def read(self, _date=None):
+    def read_from_database(self, _date=None):
+        if _date is not None:
+            self.today = _date
         tasks = self._read_file()
+        # Сортируем, прочитанную из файла информацию, по дате
         sort_tasks = sorted(tasks.items(), key=lambda x: x[0])
         if _date is not None:
             self.today = _date
+        # выбираем первые 5 предстоящие и выполненные дела
         nxt = self._to_string([task for task in sort_tasks \
                                if str(self.today) < task[0]][:5])
         previous = self._to_string([task for task in sort_tasks \
@@ -77,10 +92,12 @@ class FileJson(Base):
         return nxt, previous
 
     def _read_file(self):
+        # Читаем из файла информацию
         with open(self.path_to_file, 'r') as f:
             return json.load(f)
 
     def _to_string(self, data):
+        # Конвертируем список в строку
         res = ''
         try:
             for key, value in data:
@@ -93,24 +110,26 @@ class FileJson(Base):
 class FileSQL(Base):
 
     def __init__(self):
+        # Устанавливаем соединение с БД
         self.conn = sqlite3.connect('database.db', uri=True)
         self.cursor = self.conn.cursor()
         super().__init__()
 
-    def write(self, task, _date):
+    def write_to_database(self, task, _date):
+        # Создаем таблицу, если она не существует и вносим пользовательские данные.
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS manager
                                (date text, task text)''')
         data = _date, task
         self.cursor.execute('INSERT INTO manager VALUES(?,?)', data)
         self.conn.commit()
         self.conn.close()
-        return f'Данные успешно занесены в журнал'
 
-    def read(self, _date=None):
+    def read_from_database(self, _date=None):
         if _date is not None:
             self.today = _date
         nxt = ''
         previous = ''
+        # Читаем из БД первые 5 выполненные и предстоящие задачи
         try:
             for key, value in self.cursor.execute(
                     'SELECT date, task FROM manager \
@@ -124,161 +143,9 @@ class FileSQL(Base):
                 previous += f'{key} {value}\n'
         except sqlite3.OperationalError as err:
             raise sqlite3.OperationalError(
-                            'В вашем планировщике еще нет записей', err
+                    'В вашем планировщике еще нет записей', err
             )
         self.conn.close()
-        # nxt = '\n'.join([f'{key} {value}' for key, value in res.items() if str(self.today) < key])
-        # previous = '\n'.join([f'{key} {value}' for key, value in res.items() if str(self.today) > key])
         return nxt, previous
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def convert_date(_date):
-#     # Добавляем к введенной дате год
-#     today = date.today()
-#     try:
-#         day, month = get_date(_date)
-#         if 'послезавтра' in _date:
-#             today = today + timedelta(days=2)
-#         elif 'завтра' in _date:
-#             today = today + timedelta(days=1)
-#     except AttributeError:
-#         return today
-#     try:
-#         if today > today.replace(day=day, month=month, year=today.year):
-#             # Если дата уже прошла, то переходит на следующий год
-#             today = today.replace(day=day, month=month, year=today.year + 1)
-#         else:
-#             today = today.replace(day=day, month=month, year=today.year)
-#     except ValueError:
-#         raise ValueError('Введена неверная дата')
-#     return str(today)
-#
-#
-# def get_date(_date):
-#     day, month = list(map(int, _date.split('.')))
-#     return day, month
-
-
-
-
-
-
-# class Base(ABC):
-#
-#     def __init__(self, data=None):
-#         self.data = data
-#
-#     @
-#
-#
-#
-# class FileJson:
-#
-#     def __init__(self, data=None):
-#         # self.path_to_file = os.path.join(tempfile.gettempdir(), \
-#         #                                  'manager1.data')
-#         self.path_to_file = os.path.join('', 'manager1.data')
-#         self.data = data
-#         self.tasks = dict()
-#
-#     def write_json(self):
-#         if os.path.exists(self.path_to_file):
-#             self.tasks = self._read()
-#         with open(self.path_to_file, 'w') as f:
-#             for key, value in self.data.items():
-#                 if key not in self.tasks:
-#                     self.tasks[key] = list()
-#                 self.tasks[key].append(value)
-#             json.dump(self.tasks, f)
-#         return f
-#
-#     def read_json(self):
-#         if os.path.exists(self.path_to_file):
-#             self.tasks = self._read()
-#             return self.tasks
-#         else:
-#             raise FileNotFoundError('В вашем менеджере еще нет записей')
-#
-#     def _read(self):
-#         with open(self.path_to_file, 'r') as f:
-#             return json.load(f)
-#
-#
-# def get_date(date):
-#     day, month = list(map(int, date.split('.')))
-#     return day, month
-#
-#
-# class FileSQL:
-#
-#     def __init__(self, data=None):
-#         self.data = data
-#         self.conn = sqlite3.connect('database.db')
-#         self.cursor = self.conn.cursor()
-#
-#     def add_to_datebase(self):
-#         self.cursor.execute('''CREATE TABLE IF NOT EXISTS manager
-#                                (date text, task text)''')
-#         self.data = self.data.items()
-#         self.cursor.executemany('INSERT INTO manager VALUES(?,?)', self.data)
-#         self.conn.commit()
-#
-#     def read_from_database(self):
-#         res = OrderedDict()
-#         for key, value in self.cursor.execute('SELECT * FROM manager ORDER BY date'):
-#             res[key] = value
-#         return res
-#
-#
-#
-
-    # def convert_date(self, _date):
-    #     # Добавляем к введенной дате год
-    #     try:
-    #         day, month = self.get_date(_date)
-    #         if 'послезавтра' in _date:
-    #             self.today = self.today + timedelta(days=2)
-    #         elif 'завтра' in _date:
-    #             self.today = self.today + timedelta(days=1)
-    #     except AttributeError:
-    #         return self.today
-    #     try:
-    #         if self.today > self.today.replace(day=day, month=month, year=self.today.year):
-    #             # Если дата уже прошла, то переходит на следующий год
-    #             self.today = self.today.replace(day=day, month=month, year=self.today.year + 1)
-    #         else:
-    #             self.today = self.today.replace(day=day, month=month, year=self.today.year)
-    #     except ValueError:
-    #         raise ValueError('Введена неверная дата')
-    #     return str(self.today)
-
-    # def get_date(self, _date):
-    #     day, month = list(map(int, _date.split('.')))
-    #     return day, month
--- task_manager_input.py
+++ task_manager_input.py
@@ -1,108 +1,25 @@
 import argparse
-<<<<<<< HEAD
 from file import FileJson, FileSQL, convert_date
-=======
-# import os
-# import json
-from datetime import timedelta, date
-from File import FileJson, FileSQL, get_date
-
-
-def add_to_manager(task, _date, database=None):
-    # Передаем на запись запланированную дату и занятие в виде словаря
-    _date = convert_date(_date)
-    dct = dict()
-    dct[date] = task
-    if args.database == 'SQL' or database is None:
-        FileSQL(dct).add_to_datebase()
-    elif args.database == 'Json':
-        FileJson(dct).write_json()
-    return f'Данные успешно занесены в журнал'
-
-
-def convert_date(data):
-    # Добавляем к введенной дате год
-    day, month = get_date(data)
-    d = date.today()
-    if 'послезавтра' in data:
-        d = d + timedelta(days=2)
-    elif 'завтра' in data:
-        d = d + timedelta(days=1)
-    try:
-        if d > d.replace(day=day, month=month, year=d.year):
-            # Если дата уже прошла, то переходит на следующий год
-            d = d.replace(day=day, month=month, year=d.year + 1)
-        else:
-            d = d.replace(day=day, month=month, year=d.year)
-    except ValueError:
-        raise ValueError('Введена неверная дата')
-    return str(d)
->>>>>>> Task manager
+from file import FileJson, FileSQL
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-<<<<<<< HEAD
     parser.add_argument('-task', type=str, help='Введите задание')
     parser.add_argument('-date', type=str, help='Введите дату')
     parser.add_argument('-database', help='Введите способ хранения данных:'\
                                           'SQL или Json. По умолчанию Json')
     args = parser.parse_args()
 
-    print(args.date)
+    # Конвертируем дату из дд.мм в гггг.мм.дд
     args.date = convert_date(args.date)
-    print(args.date)
 
+    # В зависимости от пользовательского ввода, выбираем, где хранить данные
     if args.database == 'Json' or not args.database:
         writer = FileJson()
+        print(f'Данные успешно занесены в журнал')
     elif args.database == 'SQL':
         writer = FileSQL()
-    print(args.date)
-    writer.write(args.task, args.date)
+        print(f'Данные успешно занесены в журнал')
+    writer.write_to_database(args.task, args.date)
 
-
-
-
-
-
-
-#
-#
-# def add_to_manager(task, _date, database=None):
-#     # Передаем на запись запланированную дату и занятие в виде словаря
-#     _date = convert_date(_date)
-#     dct = dict()
-#     dct[date] = task
-#     if args.database == 'SQL' or database is None:
-#         FileSQL(dct).add_to_datebase()
-#     elif args.database == 'Json':
-#         FileJson(dct).write_json()
-#     return f'Данные успешно занесены в журнал'
-#
-#
-# def convert_date(data):
-#     # Добавляем к введенной дате год
-#     day, month = get_date(data)
-#     d = date.today()
-#     if 'послезавтра' in data:
-#         d = d + timedelta(days=2)
-#     elif 'завтра' in data:
-#         d = d + timedelta(days=1)
-#     try:
-#         if d > d.replace(day=day, month=month, year=d.year):
-#             # Если дата уже прошла, то переходит на следующий год
-#             d = d.replace(day=day, month=month, year=d.year + 1)
-#         else:
-#             d = d.replace(day=day, month=month, year=d.year)
-#     except ValueError:
-#         raise ValueError('Введена неверная дата')
-#     return str(d)
-=======
-    parser.add_argument('-task', type=str, help='enter task')
-    parser.add_argument('-date', help='enter data')
-    parser.add_argument('-database', help='Введите способ хранения данных:'\
-                                          'SQL или Json. По умолчанию Json')
-    args = parser.parse_args()
-    add_to_manager(args.task, args.date, args.database)
-
->>>>>>> Task manager
